9be35a108366fc1c38b770fa3e578610f83542d6
Nik <nik.pirnovar@windowslive.com>
2017-11-29 14:38:55 +0100

    Added health

---
+++ projekt-apartment/api/src/main/java/si/fri/rso/projekt/apartment/api/v1/configuration/RestProperties.java
@@ -0,0 +1,19 @@
+package si.fri.rso.projekt.apartment.api.v1.configuration;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import javax.enterprise.context.ApplicationScoped;
+
+@ConfigBundle("rest-properties")
+@ApplicationScoped
+public class RestProperties {
+
+    private boolean healthy;
+
+    public boolean isHealthy() {
+        return healthy;
+    }
+
+    public void setHealthy(boolean healthy) {
+        this.healthy = healthy;
+    }
+}
