0c3fb62be787ad798ac2f4aeb936876424321673
kknavs <karmen.knavs@gmail.com>
2018-01-09 23:24:22 +0100

    Added watch on config healthy

--- projekt-user/api/src/main/java/si/fri/rso/projekt/user/api/v1/configuration/RestProperties.java
+++ projekt-user/api/src/main/java/si/fri/rso/projekt/user/api/v1/configuration/RestProperties.java
@@ -1,12 +1,16 @@
 package si.fri.rso.projekt.user.api.v1.configuration;
 import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.cdi.ConfigValue;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ConfigBundle("rest-properties")
 @ApplicationScoped
 public class RestProperties {
+
+    @ConfigValue(watch = true)
     private boolean healthy;
+    // docker exec etcd etcdctl --endpoints //192.168.99.100:2379 set /environments/dev/services/rso-user/1.0.0/config/rest-properties/healthy true
 
     public boolean isHealthy() {
         return healthy;
